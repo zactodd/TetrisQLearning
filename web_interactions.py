853c63dc56bdad4c0dda21a7bd24a85c20b56747
Zac Todd <zactodd0@gmail.com>
2020-03-22 14:56:31 +1300

    Create web_interactions.py

---
+++ web_interactions.py
@@ -0,0 +1,21 @@
+from utils import get_chrome_driver
+from selenium.common.exceptions import NoSuchElementException
+
+DRIVER = get_chrome_driver()
+URL = "https://tetris.com/play-tetris"
+
+
+class Page:
+    def __init__(self, browser=DRIVER):
+        self.browser = self._browser_initialise(browser)
+
+    @staticmethod
+    def _browser_initialise(browser):
+        """
+        Initialise the browser.
+        :param browser: The browser prior to being initialise.
+        :return: The browser.
+        """
+        browser.get(URL)
+        return browser
+
