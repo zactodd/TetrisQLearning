0b2b965466cd42d8fbd74e7fe71e6e9c2cde67fe
Zac Todd <zactodd0@gmail.com>
2020-10-30 16:05:43 +1300

    Update board.py

--- tetris_ql/game/board.py
+++ tetris_ql/game/board.py
@@ -13,6 +13,7 @@ class Board:
         self._num_visible_tetrominos = 3
 
         self._matrix = np.zeros((self._width, self._height + self._height_padding))
+        self._previous_matrix = self._matrix.copy()
         self._I = np.identity(self._height)
 
         self.lines = 0
@@ -54,6 +55,8 @@ class Board:
         if num_lines_clears > 0:
             shifts = (self._I[p + 1: c, ] for p, c in zip((-1, *lines_cleared), (*lines_cleared, -1)))
             t = np.concatenate((np.zeros((num_lines_clears, self._height)), *shifts), axis=0)
+
+            self._previous_matrix = self._matrix.copy()
             self._matrix = np.dor(t, self._matrix)
             self._combo += 1
         else:
